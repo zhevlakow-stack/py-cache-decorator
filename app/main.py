3903785ab11f2d80ed15fdd4bb40359255023264
DESKTOP-FR144OA\Anubis <adamas87@gmail.com>
2025-10-07 17:10:20 +0800

    'Solution'

--- app/main.py
+++ app/main.py
@@ -2,5 +2,17 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    func_cache = {}
+
+    def wrapper(*args, **kwargs) -> Callable:
+        cache_key = args
+        if cache_key in func_cache:
+            print("Getting from cache")
+            return func_cache[cache_key]
+        else:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            func_cache[cache_key] = result
+            return result
+
+    return wrapper
